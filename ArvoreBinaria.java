03ec48efb62d70e126489cdfc9eb45a69d18b58b
Bruno Tontini Zanella <brunotzanella22@gmail.com>
2025-10-14 20:40:04 -0300

    Update ArvoreBinaria.java

--- ArvoreBinaria.java
+++ ArvoreBinaria.java
@@ -10,6 +10,10 @@ public class ArvoreBinaria<T> {
         this.raiz = (T) raiz;
     }
 
+    public T getRaiz(){
+        return raiz;
+    }
+
     public boolean estaVazia(){
         if (this.raiz == null){
             return true;
@@ -19,6 +23,32 @@ public class ArvoreBinaria<T> {
     }
 
     public boolean pertence(T info){
-        return pertence(raiz, info);
+        return pertence((NoArvoreBinaria<T>) raiz, info);    
+    }
+
+    private boolean pertence(NoArvoreBinaria<T> no, T info){
+        if(no == null){
+            return false;
+        }else{
+            return(no.getInfo().equals(info)) ||
+            pertence(no.getEsquerda(), info) ||
+            pertence(no.getDireita(), info);
+        }
+    }
+
+    public String toString(){
+        return "pendente";
+    }
+
+    public int contarNos(){
+        return contarNos((NoArvoreBinaria<T>) getRaiz());
+    }
+
+    private int contarNos(NoArvoreBinaria<T> no){
+        if (no == null) {
+            return 0;
+        }else{
+            return 1 + contarNos(no.getEsquerda()) + contarNos(no.getDireita());
+        }
     }
 }
